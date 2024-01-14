5a84cf01597c552d4c9a71432c6aee23ef330f2e
Jasurbek <jasurkamilov7279@gmail.com>
2024-01-14 16:18:51 +0500

    .

--- main/views.py
+++ main/views.py
@@ -1,26 +1,15 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from rest_framework.views import APIView
 from rest_framework import generics, status
-from django.http import Http404
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.encoding import force_bytes, force_str
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.conf import settings
-from django.core.mail import send_mail
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
-import random
-import telebot
 from drf_yasg.utils import swagger_auto_schema
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
 from drf_yasg import openapi
 from django.contrib.auth import update_session_auth_hash
 from django.db.models import Sum
