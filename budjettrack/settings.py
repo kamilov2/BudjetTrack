32fc3e0db489ad651350f159fccbfad405b017c2
Jasurbek <jasurkamilov7279@gmail.com>
2024-01-14 16:59:12 +0500

    .

--- budjettrack/settings.py
+++ budjettrack/settings.py
@@ -143,7 +143,7 @@ ROOT_URLCONF = 'budjettrack.urls'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
+EMAIL_PORT = 993
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'about.uz@yandex.ru'
