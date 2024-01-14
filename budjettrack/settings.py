cdb3285497949a2039526574a9ac8235582e5217
Jasurbek <jasurkamilov7279@gmail.com>
2024-01-14 17:49:06 +0500

    .

--- budjettrack/settings.py
+++ budjettrack/settings.py
@@ -141,8 +141,8 @@ SESSION_COOKIE_SAMESITE = 'Lax'
 
 ROOT_URLCONF = 'budjettrack.urls'
 
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
+EMAIL_HOST = 'simap.yandex.com'
+EMAIL_PORT = 993
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'about.uz@yandex.ru'
--- main/urls.py
+++ main/urls.py
@@ -1,22 +1,21 @@
-from django.urls import re_path , path
+from django.urls import path
 from .views import *
 from django.contrib.auth import views as auth_views
 
 app_name = "main"
 
 urlpatterns = [
-    re_path(r'^limit/$', LimitAPIView.as_view(), name='limit'),
-    re_path(r'^register/$', RegisterAPIView.as_view(), name='register'),  
-    re_path(r'^category/$', CategoryAPIView.as_view(), name='category'),
-    re_path(r'^expense/$', ExpenseAPIView.as_view(), name="expense"),
-    re_path(r'^update_password/$', UpdatePasswordAPIView.as_view(), name="update_password"),
-    re_path(r'^total_category/$' , ExpenseCategoryTotalAPIView.as_view() , name='expense_category'),
-    re_path(r'^all_expense', AllExpenseAPIView.as_view() , name="all_expense"),   
+    path('limit/', LimitAPIView.as_view(), name='limit'),
+    path('register/', RegisterAPIView.as_view(), name='register'),  
+    path('category/', CategoryAPIView.as_view(), name='category'),
+    path('expense/', ExpenseAPIView.as_view(), name="expense"),
+    path('update_password/', UpdatePasswordAPIView.as_view(), name="update_password"),
+    path('total_category/', ExpenseCategoryTotalAPIView.as_view(), name='expense_category'),
+    path('all_expense/', AllExpenseAPIView.as_view(), name="all_expense"),   
     path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
-    re_path(r'^login/$', LoginAPIView.as_view(), name="login"),
-
+    path('login/', LoginAPIView.as_view(), name="login"),
 ]
