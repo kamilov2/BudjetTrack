2aa13e12a647376c358ede820c43c438fdf53041
Jasurbek <jasurkamilov7279@gmail.com>
2024-01-14 16:51:46 +0500

    .

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import re_path , path
 from .views import *
-from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
+from django.contrib.auth import views as auth_views
 
 app_name = "main"
 
@@ -12,10 +12,10 @@ urlpatterns = [
     re_path(r'^update_password/$', UpdatePasswordAPIView.as_view(), name="update_password"),
     re_path(r'^total_category/$' , ExpenseCategoryTotalAPIView.as_view() , name='expense_category'),
     re_path(r'^all_expense', AllExpenseAPIView.as_view() , name="all_expense"),   
-    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
     re_path(r'^login/$', LoginAPIView.as_view(), name="login"),
 
